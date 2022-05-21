49530baaf026144de0493f8ed69f04b24a22580a
bassem kamel <bassemkamel.ese@gmail.com>
2022-05-21 16:12:00 +0200

    menu1 file

---
+++ menu1.sh
@@ -0,0 +1,34 @@
+#!/bin/bash 
+
+PS3="enter your choice:"
+
+dir_name="bassem"
+select c in "Create DB" "List Databases" "Connect" "Drop"
+do
+	case $REPLY in 
+		1) echo " creating -----------"
+		   . ./name_validation.sh
+		   if [ $? ]
+		   then 
+			   mkdir -p /home/bassem/iti/DBMS/$name
+		   fi
+		       	;;
+		2) echo " listing ------------" 
+			ls /home/bassem/iti/DBMS
+			;;
+
+		3) echo " connecting ----------"
+			cd /home/bassem/iti/DBMS/$dir_name
+			pwd
+		       	;;
+		4) echo " dropping --------------"
+			cd /home/bassem/iti/DBMS
+			rm -r /home/bassem/iti/DBMS/$dir_name
+		       	;; 
+
+		*) echo "invalid input" ;;
+
+	esac
+done
+
+
