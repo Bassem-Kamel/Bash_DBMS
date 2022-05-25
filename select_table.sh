90e081e163fa00cc157645680cd9e657b948cea9
AlaaAAmin <alaaamin_epm@yahoo.com>
2022-05-26 00:28:05 +0200

    fixed some bugs changelog: - fixed the bug that caused the select by column name to produce an error

--- select_table.sh
+++ select_table.sh
@@ -13,11 +13,20 @@ do
         tail -n+2 $dbpath/$table | column -t -s ":"
         ;;
 
-        2) read -p "enter the name of column: " column
+        2) read -p "enter the name of column: " columnName
         #checking if the column exists in the first row of the file > aka the columns
-        if grep -Fxq $column $dbpath/$table
+        if head -1 $dbpath/$table | grep -q $columnName
         then
-            awk 'BEGIN{FS=OFS=":"} {print $1}' $dbpath/$table
+        #getting the first line then piping it to sed to repllace the delimeter with \n
+        # so that every word in the head is on new line then piping it to nl to add line numbers
+        # then searching for the desired column and lastly piping to cut so we can get the column number
+            colNum=$(head -1 $dbpath/$table | sed 's/:/\n/g' | nl | grep $columnName | cut -f 1)
+            echo "--------------------------- $table ---------------------------"
+            cut -d: -f $colNum $dbpath/$table | head -1
+            echo "--------------------------------------------------------------"
+            cut -d: -f $colNum $dbpath/$table | tail -n+2
+            echo "--------------------------------------------------------------"
+
         else
             echo "the column name can't be found..."
         fi
