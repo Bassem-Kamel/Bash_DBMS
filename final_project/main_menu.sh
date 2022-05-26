643723d9d46d71539064b53bd4b65ed435559940
bassem kamel <bassemkamel.ese@gmail.com>
2022-05-26 21:35:38 +0200

    edit main menu

--- final_project/main_menu.sh
+++ final_project/main_menu.sh
@@ -1,15 +1,18 @@
 #!/bin/bash 
 
+#change the prompt selector and save the DBMS path into variable
 PS3="(Main menu)---> enter your choice:"
 path="$(pwd)/../DBMS"
 
 while true
 do
 
-
+	#Main menu :
 	select c in "Create DB" "List Databases" "Connect" "Drop" "Done"
 	do
-	case $REPLY in 
+	case $REPLY in
+		#Create DB : take the db name and check if it's valid 
+		#then check if the db already exists or not , if not then will be created  
 		1) echo "-------------- (creating DB) ------------------"
 		   read -p "----> enter the database name : " db_name
 		   . name_validation.sh $db_name
@@ -24,11 +27,12 @@ do
 		   fi
 		   break
 		       	;;
+		#List : just list all Databases in DBMS		   
 		2) echo "------------------------- (listing) ----------------------" 
 			ls $path
 			break
 			;;
-
+		#connect: check if the db exists or not, then change directoy inside it and run the menu2 script
 		3) echo "--------------------- (connecting) ---------------------"
 			read -p "---> enter the database name : " db
 			if [ -d "$path/$db" ]
@@ -41,8 +45,9 @@ do
 			fi
 			break
 		       	;;
+		#drop: if the db exists remove it 
 		4) echo "------------------------(dropping) ------------------------"
-			read -p "enter the database name :" db_drop
+			read -p "---> enter the database name :" db_drop
 			if [ -d $path/$db_drop ]
 			then
 				cd $path
@@ -52,7 +57,7 @@ do
 			fi
 			break
 		       	;; 
-
+		#go outside the script
 		5) echo " -------------------------(Done) --------------------------"
 			break 2
 			;;
