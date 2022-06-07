6c6c0c9c46f25173bccd31edb881f12b52d79027
bassem <bassemkamel.ese@gmail.com>
2022-06-08 01:23:55 +0200

    modify select function

--- final_project/select_table.sh
+++ final_project/select_table.sh
@@ -2,53 +2,68 @@
 
 table=$1
 dbpath=$2
+PS3="Select Menu -> enter your choice: "
 
-select c in "Select all" "Select specific column" "Select specific record" "Return to previous menu"
+while true
 do
-    case $REPLY in
-        1) hline "$table"
-        #column displays info in tabular format using -t option. the -s option specifies the delimeter
-        head -1 $dbpath/$table | column -t -s ":"
-        #passing number of spaces equal to the number of the chars as the table name
-        hline "%${#table}s"
-        tail -n+2 $dbpath/$table | column -t -s ":"
-        hline "%${#table}s"
-        ;;
 
-        2) read -p "enter the name of column: " columnName
-        #checking if the column exists in the first row of the file > aka the columns
-        if head -1 ./$table | grep -q $columnName
-        then
-        #getting the first line then piping it to sed to repllace the delimeter with \n
-        # so that every word in the head is on new line then piping it to nl to add line numbers
-        # then searching for the desired column and lastly piping to cut so we can get the column number
-            colNum=$(head -1 $dbpath/$table | sed 's/:/\n/g' | nl | grep $columnName | cut -f 1)
-            hline "$table"
-            cut -d: -f $colNum $dbpath/$table | head -1
-            hline "%${#table}s"
-            cut -d: -f $colNum $dbpath/$table | tail -n+2
-            hline "%${#table}s"
-
-        else
-            error "Error: the column name can't be found"
-        fi
-        ;;
-
-        3) read -p "enter the record data: " recordData
-        res=$(tail -n+2 $dbpath/$table | grep $recordData)
-        if [ $? -eq 0 ]
-        then
+    select c in "Select all" "Select specific column" "Select specific record" "Return to previous menu"
+    do
+        case $REPLY in
+            1) clear
             hline "$table"
             #column displays info in tabular format using -t option. the -s option specifies the delimeter
             head -1 $dbpath/$table | column -t -s ":"
+            #passing number of spaces equal to the number of the chars as the table name
             hline "%${#table}s"
-            echo -e "$res\n" | column -t -s ":"
+            tail -n+2 $dbpath/$table | column -t -s ":"
             hline "%${#table}s"
-        else
-            error "can't find the data you are looking for"
-        fi
-        ;;
-        4) break ;;
-        *) error "invalid input" ;;
-	esac
+            break
+            ;;
+
+            2) clear
+            head -1 $dbpath/$table | column -t -s ":"
+            hline "%${#table}s"
+            read -p "enter the name of column: " columnName
+            #checking if the column exists in the first row of the file > aka the columns
+            if head -1 ./$table | grep -q $columnName
+            then
+            #getting the first line then piping it to sed to repllace the delimeter with \n
+            # so that every word in the head is on new line then piping it to nl to add line numbers
+            # then searching for the desired column and lastly piping to cut so we can get the column number
+                colNum=$(head -1 $dbpath/$table | sed 's/:/\n/g' | nl | grep $columnName | cut -f 1)
+                hline "$table"
+                cut -d: -f $colNum $dbpath/$table | head -1
+                hline "%${#table}s"
+                cut -d: -f $colNum $dbpath/$table | tail -n+2
+                hline "%${#table}s"
+
+            else
+                error "Error: the column name can't be found"
+            fi
+            break
+            ;;
+
+            3) clear
+            read -p "enter the record PK : " recordData
+            # Select by primary key
+            res=$(awk -F: -v p="$recordData" '{if ($1==p) print$0}'  $dbpath/$table)
+            if [ $res ]
+            then
+                hline "$table"
+                #column displays info in tabular format using -t option. the -s option specifies the delimeter
+                head -1 $dbpath/$table | column -t -s ":"
+                hline "%${#table}s"
+                echo -e "$res\n" | column -t -s ":"
+                hline "%${#table}s"
+            else
+                error "can't find the data you are looking for"
+            fi
+            break
+            ;;
+            4) clear
+                break 2;;
+            *) error "invalid input" ;;
+        esac
+    done
 done
